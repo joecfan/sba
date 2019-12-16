a7c49a16b0ab0d9bec9510faefe891b2437a47ae
baozhuhui <baozhuh@cn.ibm.com>
2019-12-16 19:44:37 +0800

    lab03_reactjs16

--- lab03_reactjs16/smart-player/src/api/index.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3001';
-const http = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-export default http;
--- lab03_reactjs16/smart-player/src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import courseReducer from './reducers/courseReducer';
-import playListReducer from './reducers/playListReducer';
-
-export default combineReducers({
-  courses: courseReducer,
-  playlist: playListReducer,
-});
